044352c25795723c3b80e8333b0494ce21b7c930
NuclearMace <robertscalum2002@gmail.com>
2023-10-09 01:07:46 +0100

    duplicate

--- kubejs/server_scripts/disable.js
+++
@@ -1,6 +0,0 @@
-ServerEvents.recipes(event => {
-    event.remove("forcecraft:infuser/convert_time_torch")
-    event.remove("ae2:network/blocks/spatial_anchor")
-    event.remove("easy_villagers:auto_trader")
-    event.remove("easy_villagers:iron_farm")
-})

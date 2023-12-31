f6637030dc80884b27930b6171df3a4553426e83
billbertking1 <billbertking1@users.noreply.github.com>
2023-12-31 16:50:03 -0600

    stuff

--- kubejs/startup_scripts/worldgen.js
+++ kubejs/startup_scripts/worldgen.js
@@ -7,7 +7,15 @@ WorldgenEvents.remove(event => {
       "railcraft:tin_ore",
       "railcraft:deepslate_tin_ore",
       "railcraft:zinc_ore",
-      "railcraft:deepslate_zinc_ore"
+      "railcraft:deepslate_zinc_ore",
+      "railcraft:nickel_ore",
+      "railcraft:deepslate_nickel_ore",
+      "railcraft:sulfur_ore",
+      "railcraft:deepslate_sulfur_ore",
+      "railcraft:silver_ore",
+      "railcraft:deepslate_silver_ore",
+      "immersiveengineering:ore_nickel",
+      "immersiveengineering:deepslate_ore_nickel"
       ]
     })
 })

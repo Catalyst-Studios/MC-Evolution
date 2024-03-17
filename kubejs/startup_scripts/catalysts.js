078cd22c239132be015eed2009b5b600468a159b
billbertking1 <billbertking1@users.noreply.github.com>
2024-03-17 12:29:10 -0500

    add catalyst items in game w/wip textures

---
+++ kubejs/startup_scripts/catalysts.js
@@ -0,0 +1,9 @@
+StartupEvents.registry('item', e => {
+    e.create('storage_catalyst').displayName('Storage Catalyst').texture('kubejs:item/storage_catalyst')
+    e.create('innovation_catalyst').displayName('Innovation Catalyst').texture('kubejs:item/innovation_catalyst')
+    e.create('adventure_catalyst').displayName('Adventure Catalyst').texture('kubejs:item/adventure_catalyst')
+    e.create('boss_catalyst').displayName('Boss Catalyst').texture('kubejs:item/boss_catalyst')
+    e.create('growth_catalyst').displayName('Growth Catalyst').texture('kubejs:item/growth_catalyst')
+    e.create('power_catalyst').displayName('Power Catalyst').texture('kubejs:item/power_catalyst')
+    e.create('magic_catalyst').displayName('Magic Catalyst').texture('kubejs:item/magic_catalyst')
+})

9945480a1ae15a8f96f802ca4d7c694f905c1b6c
billbertking1 <billbertking1@users.noreply.github.com>
2024-09-12 21:56:18 -0500

    EMI support, add catalysts, add tags to make quest dev easier

---
+++ kubejs/server_scripts/tags.js
@@ -0,0 +1,6 @@
+ServerEvents.tags('item', catalyst => {
+    catalyst.add('c:armors/diamond', 'minecraft:diamond_helmet')
+    catalyst.add('c:armors/diamond', 'minecraft:diamond_chestplate')
+    catalyst.add('c:armors/diamond', 'minecraft:diamond_leggings')
+    catalyst.add('c:armors/diamond', 'minecraft:diamond_boots')
+})

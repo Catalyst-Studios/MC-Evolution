be5f32b4f1cc70f20acc839964e0bc4549d385fa
RadzRatz <106654292+RadzRatz@users.noreply.github.com>
2024-09-16 10:15:49 -0600

    Chnages to them quests from Useful Mods questline
    - Added 2 new tags for them tiny coals to ease the making of the quest
    
    - Added pumps to ease the making of the quest
    
    - Added EnderIO quests to Useful Mods, Travel anchor quest needs to be edited still
    
    - Added Everlasting abilities quest and another one for the Tiny Coals
    
    - Added a Wooden jetpack to Jetpacks quest
    
    - Removed warp stone from amazing blocks/waystone quest and readded it back into useful tools
    
    - Edited some quests like Logistics, Resources quests
    
    - Only Useful Mods questline was edited
    
    - Added the new titles, desc and subts to en_us.snbt

--- kubejs/server_scripts/tags.js
+++ kubejs/server_scripts/tags.js
@@ -3,4 +3,15 @@ ServerEvents.tags('item', catalyst => {
     catalyst.add('c:armors/diamond', 'minecraft:diamond_chestplate')
     catalyst.add('c:armors/diamond', 'minecraft:diamond_leggings')
     catalyst.add('c:armors/diamond', 'minecraft:diamond_boots')
+
+    //Fluid Pumps
+    catalyst.add('c:block/fluidpump', 'mekanism:electric_pump')
+    catalyst.add('c:block/fluidpump', 'rangedpumps:pump')
+    catalyst.add('c:block/fluidpump', 'energizedpower:fluid_pump')
+    catalyst.add('c:block/fluidpump', 'xycraft_machines:hydro_pump')
+    
+    //Tiny (Char)Coal
+    catalyst.add('c:fueltype/tinycoals', 'kubejs:tiny_charcoal')
+    catalyst.add('c:fueltype/tinycoals', 'kubejs:tiny_coal')
+
 })
